3988447188e48f4f9749e586079037ef52c59f8d
oldman3483 <qazws3483@gmail.com>
2025-02-04 17:04:45 +0800

    upload the initial files

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///test.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+
+db.create_all()
+
+@app.route('/add', methods=['POST'])
+def add_data():
+    data = request.json.get('content')
+    if not data:
+        return jsonify({'error': 'No content provided'}), 400
+    new_entry = Data(content=data)
+    db.session.add(new_entry)
+    db.session.commit()
+    return jsonify({'message': 'Data added!', 'id': new_entry.id})
+
+@app.route('/get', methods=['GET'])
+def get_data():
+    data_list = Data.query.all()
+    return jsonify([{'id': d.id, 'content': d.content} for d in data_list])
+
+if __name__ == '__main__':
+    app.run(debug=True)
